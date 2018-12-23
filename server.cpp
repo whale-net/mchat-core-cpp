72b44b60513016e9099910537ddae202b4921883
qmastermoo <alex.harding@whale-net.net>
2018-12-23 13:40:17 -0500

    going to convert server to namespace

--- server.cpp
+++ server.cpp
@@ -10,12 +10,22 @@ HTTPServer::HTTPServer(char * addr_, char * port_){
 	//		 Most important feature is to have it initialized so
 	// 		 we don't have to worry about thread-safety
 	handlers.emplace("example", std::make_unique<endpoint::Example>());
-	handlers["example"]->handle("GET");
-	handlers["example"]->handle("POST");
+	// handlers["example"]->handle("GET");
+	// handlers["example"]->handle("POST");
 
-	ioc = stdioc{1};
+	
 }
 
 void HTTPServer::run(){
+	boost::asio::io_context ioc{1};
+	tcp::acceptor acceptor{ioc, {addr, port}};
+	while (true){
+		// block until new client request
+		tcp::socket socket{ioc};
+		acceptor.accept(socket);
 
+		// std::thread(std::bind{
+		// 	&do
+		// })
+	}
 }
--- server.h
+++ server.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
@@ -25,12 +26,16 @@ public:
 	void run();
 
 private:
+	//NOTE: these are moved into run()
+	// // io context needed for everything boost/networking
+	// boost::asio::io_context ioc;
 
-	boost::asio::io_context ioc;
+	// // acceptor that will listen for new requests
+	// tcp::acceptor acceptor;
 
 	// Mapping of api endpoint to a function
 	// the /api/version<num>/ is implicit and not stored
-	std::unordered_map<std::string, std::unique_ptr<API_Endpoint>> handlers;
+	static std::unordered_map<std::string, std::unique_ptr<API_Endpoint>> handlers;
 
 
 	// Address from which the server will run
