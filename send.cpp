e4ac1244bf4a6ea1a5bde9d221b57b69e8d39fa3
qmastermoo <alex.harding@whale-net.net>
2018-12-29 17:32:50 -0500

    adding send function, templates are hard

--- send.cpp
+++ send.cpp
@@ -1,21 +1,36 @@
 #include "send.h"
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl.hpp>
 
-template<class Stream>
-Sender<Stream>::Sender(Stream& out_stream, 
-							bool& close, 
-							beast::error_code ec){
-	this->out_stream = out_stream;
-	this->close = close;
-	this->ec = ec;
+
+
+namespace beast = boost::beast;
+namespace ssl = boost::asio::ssl;
+using tcp = boost::asio::ip::tcp;
+
+namespace HTTPServer {
+	template<class Stream>
+	Sender<Stream>::Sender(Stream& out_stream, 
+								bool& close, 
+								beast::error_code ec){
+		this->out_stream = out_stream;
+		this->close = close;
+		this->ec = ec;
+	}
+
+	template<class Stream>
+	template<bool isRequest, class Body>
+	void Sender<Stream>::send(http::message<isRequest, Body>& msg) const {
+		close = msg.need_eof();
+
+		// constify for write
+		http::serializer<isRequest, Body> serial_msg{msg};
+		http::write(out_stream, serial_msg, ec);
+	}
+
+	
 }
 
-template<class Stream>
-template<bool isRequest, class Body>
-void Sender<Stream>::send(http::message<isRequest, Body>& msg) const {
-	close = msg.need_eof();
-
-	// constify for write
-	http::serializer<isRequest, Body> serial_msg{msg};
-	http::write(out_stream, serial_msg, ec);
-}
--- send.h
+++ send.h
@@ -13,28 +13,38 @@ namespace http = beast::http;
 
 namespace HTTPServer {
 
-class Sender;
+	template<class Stream>
+	class Sender {
+	public:
+		Sender(Stream& out_stream, bool& close, beast::error_code ec){
+			this->out_stream = out_stream;
+			this->close = close;
+			this->ec = ec;
+		}
 
+		template<bool isRequest, class Body>
+		void send(http::message<isRequest, Body>& msg) const {
+			close = msg.need_eof();
+
+			// constify for write
+			http::serializer<isRequest, Body> serial_msg{msg};
+			http::write(out_stream, serial_msg, ec);
+
+		}
+
+	private:
+		// Output stream for writing response
+		Stream out_stream;
+
+		// Connection closed semantic bool
+		// true when should close connection after sending
+		bool& close;
+
+		// shared error code
+		beast::error_code& ec;
+	};
 } // namespace HTTPServer
 
-template<class Stream>
-class Sender {
-public:
-	Sender(Stream& out_stream, bool& close, beast::error_code ec);
 
-	template<bool isRequest, class Body>
-	inline void send(http::message<isRequest, Body>& msg) const;
-
-private:
-	// Output stream for writing response
-	Stream out_stream;
-
-	// Connection closed semantic bool
-	// true when should close connection after sending
-	bool& close;
-
-	// shared error code
-	beast::error_code& ec;
-};
 
 #endif // SEND_H
